5217b9965fdb64a70e2ab5b551167edf11af5806
KimWooSung <kipsong133@gmail.com>
2022-02-09 10:51:59 +0900

    Initial Commit

---
+++ SwiftUI_WorkingData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_WorkingData
+//
+//  Created by 김우성 on 2022/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_WorkingData/SwiftUI_WorkingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WorkingDataApp.swift
+//  SwiftUI_WorkingData
+//
+//  Created by 김우성 on 2022/02/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WorkingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
